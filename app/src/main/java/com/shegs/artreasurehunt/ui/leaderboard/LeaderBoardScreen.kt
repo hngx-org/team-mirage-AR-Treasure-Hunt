0bfdf2ff01308f3d07d8a974fd6ea9383182e436
ani <godwin@istrategytech.com>
2023-10-20 14:49:54 +0100

    wip: leaderboard

--- app/src/main/java/com/shegs/artreasurehunt/ui/leaderboard/LeaderBoardScreen.kt
+++ app/src/main/java/com/shegs/artreasurehunt/ui/leaderboard/LeaderBoardScreen.kt
@@ -1,24 +1,21 @@
 package com.shegs.artreasurehunt.ui.leaderboard
 
 import android.annotation.SuppressLint
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
@@ -30,6 +27,7 @@ import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.shegs.artreasurehunt.R
@@ -47,15 +45,15 @@ fun LeaderBoardScreen(viewModel: LeaderBoardViewModel, navController: NavControl
         SnackbarHostState()
     }
     Scaffold(
-        topBar = {
-            TopAppBar(title = { Text("LeaderBoard") }, navigationIcon = {
-                IconButton(onClick = {
-                    navController.popBackStack()
-                }) {
-                    Icon(Icons.Filled.ArrowBack, contentDescription = null)
-                }
-            })
-        },
+//        topBar = {
+//            TopAppBar(title = { Text("LeaderBoard") }, navigationIcon = {
+//                IconButton(onClick = {
+//                    navController.popBackStack()
+//                }) {
+//                    Icon(Icons.Filled.ArrowBack, contentDescription = null)
+//                }
+//            })
+//        },
         snackbarHost = {
             SnackbarHost(snackBarHostState)
         }
@@ -100,6 +98,34 @@ fun LeaderBoardListScreen(
                 }
             } else {
                 LazyColumn {
+                    item{
+                        Row(
+                            modifier = Modifier.padding(all = 12.dp),
+                        ) {
+                            Text(
+                                text = "email",
+                                textAlign = TextAlign.Start,
+                                color = MaterialTheme.colorScheme.onPrimary,
+                                maxLines = 1,
+                                overflow = TextOverflow.Ellipsis,
+                                fontSize = 12.sp,
+                                fontFamily = FontFamily(Font(R.font.poppins_medium)),
+                                lineHeight = 16.sp
+                            )
+                            Spacer(modifier = Modifier.width(8.dp))
+                            Text(
+                                text = "score",
+                                textAlign = TextAlign.Start,
+                                color = MaterialTheme.colorScheme.onPrimary,
+                                maxLines = 1,
+                                overflow = TextOverflow.Ellipsis,
+                                fontSize = 12.sp,
+                                fontFamily = FontFamily(Font(R.font.poppins_medium)),
+                                lineHeight = 16.sp
+                            )
+                        }
+                    }
+
                     items(leaderBoard) { leaderBoard ->
                         LeaderBoardItem(leaderBoard)
                     }
@@ -118,7 +144,7 @@ fun LeaderBoardListScreen(
 @Composable
 fun LeaderBoardItem(leaderBoard: LeaderBoardModel) {
         Row(
-            horizontalArrangement = Arrangement.SpaceBetween
+            modifier = Modifier.padding(all = 12.dp),
         ) {
             Text(
                 text = leaderBoard.email,
@@ -130,7 +156,7 @@ fun LeaderBoardItem(leaderBoard: LeaderBoardModel) {
                 fontFamily = FontFamily(Font(R.font.poppins_medium)),
                 lineHeight = 16.sp
             )
-
+            Spacer(modifier = Modifier.width(8.dp))
             Text(
                 text = "${leaderBoard.score}",
                 textAlign = TextAlign.Start,
